ad32b4ee9d94b45f258584da9cb084628f84aaff
Joel Kinman <joel.kinman@gmail.com>
2016-04-14 14:16:32 -0700

    add shell app

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Wit = require('./node_modules/node-wit').Wit;
+
+const token = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/template.js <wit-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+
+const actions = {
+  say: (sessionId, msg, cb) => {
+    console.log(msg);
+    cb();
+  },
+  merge: (context, entities, cb) => {
+    cb(context);
+  },
+  error: (sessionid, msg) => {
+    console.log('Oops, I don\'t know what to do.');
+  },
+  'find-venue': (context, cb ) => {
+    console.log( context );
+    console.log( cb );
+    cb(context);
+
+  }
+};
+
+const client = new Wit(token, actions);
+client.interactive();
