9178fe663d4be4c159e9eeaad9028db90d42bcb5
Joel Kinman <joel.kinman@gmail.com>
2016-04-15 15:24:20 -0700

    adding actions back

--- bots/witbot.js
+++ bots/witbot.js
@@ -15,54 +15,19 @@ const firstEntityValue = (entities, entity) => {
   return typeof val === 'object' ? val.value : val;
 };
 
+const actions = {
+  say: (sessionId, context, message, cb) => {
+    console.log(message);
+    cb();
+  },
+  merge: (sessionId, context, entities, message, cb) => {
+    cb(context);
+  },
+  error: (sessionId, context, error) => {
+    console.log(error.message);
+  },
+};
 
-// const actions = {
-//   say: (sessionId, msg, cb) => {
-//     console.log(msg);
-//     cb();
-//   },
-//   merge: (sessionId, context, entities, message, cb) => {
-//     // console.log( context, entities );
-//     const intent = firstEntityValue(entities, 'intent');
-//     if(intent) {
-//       context.intent = intent;
-//     }
-//
-//     cb(context);
-//   },
-//
-//   error: (sessionid, msg) => {
-//     console.log('Oops, I don\'t know what to do.');
-//   },
-//
-//   'find-venue': (sessionId, context, cb ) => {
-//
-//     yelp.search({
-//       term: context.intent,
-//       location: 'Vancouver canada',
-//       limit: 20,
-//       sort: 0,
-//       radius_filter: 2000
-//     })
-//     .then(function (data) {
-//       //get a random selection
-//       let spots = data.businesses;
-//       var randBusiness = spots[Math.floor(Math.random() * spots.length)];
-//
-//       console.log( `${randBusiness.name} ${randBusiness.rating} stars`);
-//       console.log(randBusiness.location.display_address);
-//
-//       cb(context);
-//     })
-//     .catch(function (err) {
-//       console.error(err);
-//       cb(context);
-//     });
-//     console.log( context );
-//
-//   }
-// };
-
-const client = new Wit(token );
+const client = new Wit( token, actions );
 
 module.exports = client;
