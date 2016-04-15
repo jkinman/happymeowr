b4e1b2379d2133a141d43acdf6e68fb05f290fdd
Joel Kinman <joel.kinman@gmail.com>
2016-04-15 11:37:45 -0700

    renamed routes

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var bots = require('./routes/botMessageHooks');
 
 var app = express();
 console.log( `express started` );
@@ -37,40 +39,47 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+// app.use('/users', users);
+console.log( `loaded routes` );
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+// console.log( `setup 404` );
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  console.log( `found env=development` );
+// if (app.get('env') === 'development') {
+//   console.log( `found env=development` );
+//
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+//
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+// Start server
+server.listen( 8080, function() {
+	console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
-
 
 module.exports = app;
--- routes/botMessageHooks.js
+++ routes/botMessageHooks.js
@@ -6,4 +6,13 @@ router.get('/fbbot', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/slackbot', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/kikbot', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
 module.exports = router;
