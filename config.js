4e19de690297f0ab41375d76faa7021e175705b8
Joel Kinman <joel.kinman@gmail.com>
2016-04-14 17:01:56 -0700

    added deps and yelp API info

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports(
+  let config= {
+    yelp:{
+      key:	'Xl-Fu3VgSH347wuaGudkPA',
+      secret:	'_Z-eP5uOy2b1c2cc0l5LyaW_IHU',
+      token:	'NMLuRkyVpNOz4Ydcqsoky7wmao9H08qe',
+      tokenSecret:	'7YHP0BPxvRR3XSqRSsBK4eMTS7k',
+    }
+  };
+  
+)
