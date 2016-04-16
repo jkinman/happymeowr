ee09b88663c8a9ba3617aa5385e82dfee7ce5931
Joel Kinman <joel.kinman@gmail.com>
2016-04-15 19:05:01 -0700

    moved yelp

--- bots/facebookBot.js
+++ bots/facebookBot.js
@@ -3,8 +3,16 @@ var exports = module.exports = {};
 var express = require('express');
 var app = express();
 var wit = require( './witbot' );
-var yelp = require( '../api/venueFinder.js' );
+// var yelp = require( '../api/venueFinder.js' );
 const request = require('request');
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+  consumer_key: 'Xl-Fu3VgSH347wuaGudkPA',
+  consumer_secret: '_Z-eP5uOy2b1c2cc0l5LyaW_IHU',
+  token: 'NMLuRkyVpNOz4Ydcqsoky7wmao9H08qe',
+  token_secret: '7YHP0BPxvRR3XSqRSsBK4eMTS7k',
+});
 
 exports.verify = (req, res ) => {
   if (req.query['hub.verify_token'] === 'i_fell_asleep_on_my_keyboard') {
